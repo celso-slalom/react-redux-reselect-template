316d92999c6526b8fb8423824e219e18f5bfaa98
Celso Moraes <cemarte83@gmail.com>
2018-09-07 15:28:34 -0400

    adding material-ui

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,24 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+const HomeLink = (props: any) => (<Link to="/" {...props} />);
+const TodosLink = (props: any) => (<Link to="/todos" {...props} />);
+
 const Navbar = () => (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/todos">Todos</Link>
-    </nav>
+    <AppBar position="static" color="default">
+        <Toolbar>
+            <Button component={HomeLink}>
+                <Typography>Home</Typography>
+            </Button>
+            <Button component={TodosLink} >
+                <Typography>Todos</Typography>
+            </Button>
+        </Toolbar>
+    </AppBar>
 );
 
 
--- src/TodosPage.tsx
+++ src/TodosPage.tsx
@@ -1,13 +1,25 @@
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import CommentIcon from '@material-ui/icons/Comment';
 import * as React from 'react';
 
 const TodosPage = () => (
     <React.Fragment>
-        <ul>
-            <li>Todos 1</li>
-            <li>Todos 22</li>
-            <li>Todos3 3</li>
-
-        </ul>
+        <List>
+            <ListItem><Checkbox checked={false} />
+                <ListItemText primary={`Line item 1`} />
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Comments">
+                        <CommentIcon />
+                    </IconButton>
+                </ListItemSecondaryAction></ListItem>
+            <ListItem>Todos 22</ListItem>
+            <ListItem>Todos3 3</ListItem>
+        </List>
     </React.Fragment>
 );
 
